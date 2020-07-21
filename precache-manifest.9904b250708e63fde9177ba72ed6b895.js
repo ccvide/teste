68480d810c1562cc5ed26fa39ca445123c488512
ccvide <68450613+ccvide@users.noreply.github.com>
2020-07-21 11:32:09 -0300

    Add files via upload

---
+++ precache-manifest.9904b250708e63fde9177ba72ed6b895.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d4ae56d542a0797880e5784c8dcd73c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86648592a5ff21c4deb0",
+    "url": "/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "9e5a9476a6282e8ea0d5",
+    "url": "/static/css/main.f628ca1b.chunk.css"
+  },
+  {
+    "revision": "86648592a5ff21c4deb0",
+    "url": "/static/js/2.2f7511c4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2f7511c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e5a9476a6282e8ea0d5",
+    "url": "/static/js/main.dfdcf620.chunk.js"
+  },
+  {
+    "revision": "f02c66110702f9ba513e",
+    "url": "/static/js/runtime-main.7eee58f4.js"
+  }
+]);
